9ab6abdda11827f48b519c8bf6ad26df4446fdb8
BireshMishra <mishrabiresh@gmail.com>
2022-01-27 00:25:23 +0530

    1 file modified

--- src/containers/PokemonDetails.js
+++ src/containers/PokemonDetails.js
@@ -1,33 +1,94 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
+import { POKEMON_API_URL } from '../config';
+import {
+  CircularProgress,
+  Box,
+  withStyles,
+  Typography,
+} from '@material-ui/core';
 
-class PokemonDetails extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pokemon: null,
-    };
-    console.log(this.props);
-  }
+const styles = (theme) => ({
+  pokedexContainer: {
+    height: '80vh',
+    backgroundColor: 'black',
+    color: 'white',
+    marginTop: 75,
+    textAlign: 'center',
+    borderRadius: 5,
+    paddingTop: 30,
+  },
+  textTitle: {
+    textTransform: 'uppercase',
+    fontFamily: 'fantasy',
+  },
+  pokemonImage: {
+    width: '170px',
+    height: '170px',
+  },
+});
 
-  componentDidMount() {
-    const { match } = this.props;
-    console.log(match?.params);
-    //const { id } = match?.params;
+function PokemonDetails(props) {
+  const { classes } = props;
+  let { id } = useParams();
+  const [pokemon, setPokemon] = useState(null);
+  console.log(id);
 
-    // axios.get(POKEMON_API_URL + '/' + id).then((response) => {
-    //   if (response.status >= 200 && response.status < 300) {
-    //     this.setState({ pokemon: response.data });
-    //   }
-    // });
-  }
+  useEffect(() => {
+    axios.get(POKEMON_API_URL + '/' + id).then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        console.log(response.data);
+        setPokemon(response.data);
+      }
+    });
+  }, []);
 
-  render() {
-    return <div style={{ marginTop: 200 }}>Hi</div>;
+  if (pokemon) {
+    const { name, sprites } = pokemon;
+    return (
+      <Box>
+        <Box className={classes.pokedexContainer}>
+          <Typography className={classes.textTitle} variant="h1">
+            {name}
+          </Typography>
+          <img className={classes.pokemonImage} src={sprites.front_default} />
+        </Box>
+      </Box>
+    );
+  } else {
+    return <CircularProgress />;
   }
 }
 
-const PokemonDetailsWithRouter = useParams(PokemonDetails);
+export default withStyles(styles)(PokemonDetails);
 
-export default PokemonDetailsWithRouter;
+// class PokemonDetails extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       pokemon: null,
+//     };
+//     console.log(this.props);
+//   }
+
+//   componentDidMount() {
+//     const { match } = this.props;
+//     console.log(match?.params);
+//     //const { id } = match?.params;
+
+//     // axios.get(POKEMON_API_URL + '/' + id).then((response) => {
+//     //   if (response.status >= 200 && response.status < 300) {
+//     //     this.setState({ pokemon: response.data });
+//     //   }
+//     // });
+//   }
+
+//   render() {
+//     return <div style={{ marginTop: 200 }}>Hi</div>;
+//   }
+// }
+
+// const PokemonDetailsWithRouter = useParams(PokemonDetails);
+
+// export default PokemonDetailsWithRouter;
